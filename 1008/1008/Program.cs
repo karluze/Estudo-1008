d5dff21b2ba612e1874d0472a65f5853466e7cca
karluze <karluzem@gmail.com>
2022-02-14 15:56:59 -0300

    Problema 1008

---
+++ 1008/1008/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace _1008
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, NUMBER, SALARY;
+
+            a = double.Parse(Console.ReadLine());
+            b = double.Parse(Console.ReadLine());
+            c = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            NUMBER = a;
+            SALARY = b * c;
+
+            Console.WriteLine("NUMBER = " + NUMBER);
+            Console.WriteLine("SALARY = U$ " + SALARY.ToString("F2"));
+
+
+
+            //Console.ReadLine();
+        }
+    }
+}
